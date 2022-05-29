ddd21707f47a7326a0de7a0347cf4f22a1e2f056
Dmytro Feschenko <korop.karas777@gmail.com>
2022-05-29 22:51:35 +0300

    lr2

--- lr2/LR_2_task_2_2.py
+++ lr2/LR_2_task_2_2.py
@@ -1,7 +1,7 @@
-
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
 
@@ -24,11 +24,11 @@ with open(input_file, 'r') as f:
         data = line[:-1].split(', ')
         income_class = data[-1]
         if income_class == '<=50K' and count_class1 < max_datapoints:
-            X.append(data)
+            X.append(data);
             count_class1 += 1
 
         if income_class == '>50K' and count_class2 < max_datapoints:
-            X.append(data)
+            X.append(data);
             count_class2 += 1
 
 X = np.array(X)
@@ -46,7 +46,7 @@ for i, item in enumerate(X[0]):
 X = X_encoded[:, :-1].astype(int)
 y = X_encoded[:, -1].astype(int)
 
-classifier = OneVsOneClassifier(LinearSVC(random_state=0))
+classifier = OneVsOneClassifier(SVC(kernel='rbf', max_iter=10000))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 classifier.fit(X_train, y_train)
 y_test_pred = classifier.predict(X_test)
@@ -63,7 +63,9 @@ print("Recall score: " + str(round(100 * recall.mean(), 2)) + "%")
 f1 = cross_val_score(classifier, X, y, scoring='f1_weighted', cv=3)
 print("F1 score: " + str(round(100 * f1.mean(), 2)) + "%")
 
-input_data = ['37', 'Private', '215646', 'HS-grad', '9', 'Never-married', 'Handlers-cleaners', 'Not-in-family', 'White', 'Male', '0', '0', '40', 'United-States']
+input_data = ['37', 'Private', '215646', 'HS-grad', '9', 'Never-married', 'Handlers-cleaners', 'Not-in-family', 'White',
+              'Male',
+              '0', '0', '40', 'United-States']
 
 input_data_encoded = [-1] * len(input_data)
 count = 0
@@ -76,6 +78,5 @@ for i, item in enumerate(input_data):
         count += 1
 
 input_data_encoded = np.array(input_data_encoded)
-
 predicted_class = classifier.predict([input_data_encoded])
 print(label_encoder[-1].inverse_transform(predicted_class)[0])
